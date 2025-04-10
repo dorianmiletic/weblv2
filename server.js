f03f65d722f66d2a8a288e5f090d4874219e0555
dorianmiletic <dorian.miletic5@gmail.com>
2025-04-10 10:59:34 +0200

    Small fix to server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
 res.send('Pozdrav sa Railway servera!');
 });
